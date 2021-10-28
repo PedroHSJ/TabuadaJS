c17db1a41410c043832bfb57986ff7630d684b6d
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2021-10-28 15:04:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function tabuada(){
         let n = Number(num.value)
         tab.innerHTML = ''
         for( let c = 1; c <= 10 ; c++) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
+            
+            tab.innerHTML += `${n} x ${c} = ${n*c}<br>`
+            
             
         }
     }
